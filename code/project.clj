38d6602577bd100bb4651dbc6c0854404c6df9d6
Jason Whitlark <jasonw@clearstorydata.com>
2018-04-09 18:23:59 -0700

    Removed out of date code samples.

--- code/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject hey "0.0.1"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [ring/ring-core "1.6.3"]
-                 [ring/ring-jetty-adapter "1.6.3"]
-                 ]
-  :main hey.core)
